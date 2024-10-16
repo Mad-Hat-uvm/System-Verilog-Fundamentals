683dd45a311f2de5e98f8f400cf5b7a58c934e04
Mad-Hat-uvm <yamini.bitmesra@gmail.com>
2024-10-16 14:30:40 -0700

    Using inside keyword in the constraint

--- Randomization/constraint_1.sv
+++ Randomization/constraint_1.sv
@@ -1,1 +1,22 @@
+class rand_var;
+    rand logic [2:0] v; //0 to 7, including 7
+
+    constraint c1 {
+        v inside {[0:5]}; //0 to 5, including 5
+    }
+endclass
+
+module tb;
+    rand_var r_var;
+
+    initial begin
+        r_var = new();
+        
+        for(int i = 0; i < 10; i++) begin
+            $display("Round: %0d", i);
+            //if r_var.randomize is successful
+            $display("Randomize: %0d: r_var.v: %0d", r_var.randomize(), r_var.v);
+        end
+    end
+endmodule
 
