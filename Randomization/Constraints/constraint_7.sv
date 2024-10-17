02696d3c560f9fc1664adac9433451998d37d483
Mad-Hat-uvm <yamini.bitmesra@gmail.com>
2024-10-17 16:35:20 -0700

    Implication constraint example

---
+++ Randomization/Constraints/constraint_7.sv
@@ -0,0 +1,20 @@
+class randClass;
+    rand bit a, b; 
+
+    constraint c2 {
+      (a == 1) -> (b == 1);
+    }
+endclass
+
+module tb;
+    randClass rc;
+
+    initial begin
+        rc = new();
+        
+        repeat(10) begin
+           rc.randomize();
+           $display("a: %0d, b: %0d", rc.a, rc.b);
+        end
+    end
+endmodule
