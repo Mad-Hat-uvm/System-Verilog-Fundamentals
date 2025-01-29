c19702bd71e8e3e2cc306dd221db12902f38f29b
Mad-Hat-uvm <yamini.bitmesra@gmail.com>
2025-01-29 15:55:59 -0800

    Static property and Static method example

--- SV_essentials/static classes and methods/example_1.sv
+++ SV_essentials/static classes and methods/example_1.sv
@@ -1,1 +1,27 @@
+class Car;
+    static int totalCars;  //STATIC property
 
+    //Constructor
+    function new();
+        incrementTotalCars();
+    endfunction
+
+    static function void incrementTotalCars();
+        totalCars++;
+    endfunction
+
+    static function int getTotalCars();
+        return totalCars;
+    endfunction
+
+endclass
+
+module test;
+    initial begin
+        Car c1 = new();
+        Car c2 = new();
+        Car c3 = new();
+
+        $display("Total Cars: %0d", Car::getTotalCars());
+    end
+endmodule
